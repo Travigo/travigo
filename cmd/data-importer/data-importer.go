70c9621c3a96617ea3af63c2d6235ab0461f356f
Aaron Claydon <claydon.aaron@gmail.com>
2022-01-07 21:50:26 +0000

    Improve CLI by removing copy pasted file handling code

--- cmd/data-importer/data-importer.go
+++ cmd/data-importer/data-importer.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"time"
@@ -39,32 +40,77 @@ func main() {
 	notify_client.Setup()
 
 	app := &cli.App{
-		Name: "data-importer",
+		Name:        "data-importer",
+		Description: "Manages ingesting and verifying data in BritBus",
 		Commands: []*cli.Command{
 			{
-				Name:  "naptan",
-				Usage: "NaPTAN stop location data",
-				Subcommands: []*cli.Command{
-					{
-						Name:      "import-file",
-						Usage:     "import a local XML file",
-						ArgsUsage: "<file path>",
-						Action: func(c *cli.Context) error {
-							if c.Args().Len() == 0 {
-								return errors.New("file path must be provided")
-							}
+				Name:      "import",
+				Usage:     "Import a dataset into BritBus",
+				ArgsUsage: "<format> <source>",
+				Action: func(c *cli.Context) error {
+					if c.Args().Len() != 2 {
+						return errors.New("<format> and <source> must be provided")
+					}
 
-							filePath := c.Args().Get(0)
+					format := c.Args().Get(0)
+					source := c.Args().Get(1)
 
-							log.Info().Msgf("NaPTAN file import from %s", filePath)
+					dataFiles := []DataFile{}
 
-							file, err := os.Open(filePath)
-							if err != nil {
-								log.Fatal().Err(err)
-							}
-							defer file.Close()
+					// Check if the source is a URL and load the http client stream if it is
+					if isValidUrl(source) {
+						pretty.Println("This is a HTTP file")
 
-							naptanDoc, err := naptan.ParseXMLFile(file, naptan.BusFilter)
+						resp, err := http.Get(source)
+
+						if err != nil {
+							log.Fatal().Err(err)
+						}
+						defer resp.Body.Close()
+
+						dataFiles = append(dataFiles, DataFile{
+							Name:   source,
+							Reader: resp.Body,
+						})
+					}
+
+					// Ccheck if its an XML file or ZIP file
+					fileExtension := filepath.Ext(source)
+					if fileExtension == ".xml" {
+						file, err := os.Open(source)
+						if err != nil {
+							log.Fatal().Err(err)
+						}
+						defer file.Close()
+
+						dataFiles = append(dataFiles, DataFile{
+							Name:   source,
+							Reader: file,
+						})
+					} else if fileExtension == ".zip" {
+						archive, err := zip.OpenReader(source)
+						if err != nil {
+							panic(err)
+						}
+						defer archive.Close()
+
+						for _, zipFile := range archive.File {
+							file, _ := zipFile.Open()
+
+							dataFiles = append(dataFiles, DataFile{
+								Name:   fmt.Sprintf("%s:%s", source, zipFile.Name),
+								Reader: file,
+							})
+						}
+					} else {
+						return errors.New(fmt.Sprintf("Unsupport file extension %s", fileExtension))
+					}
+
+					for _, dataFile := range dataFiles {
+						switch format {
+						case "naptan":
+							log.Info().Msgf("NaPTAN file import from %s", dataFile.Name)
+							naptanDoc, err := naptan.ParseXMLFile(dataFile.Reader, naptan.BusFilter)
 
 							if err != nil {
 								return err
@@ -72,191 +118,37 @@ func main() {
 
 							naptanDoc.ImportIntoMongoAsCTDF(&ctdf.DataSource{
 								Provider: "Department of Transport",
-								Dataset:  fmt.Sprintf("local-file:%s", filePath),
+								Dataset:  dataFile.Name,
 							})
-
-							return nil
-						},
-					},
-					{
-						Name:      "import-http",
-						Usage:     "import a remote XML file over HTTP",
-						ArgsUsage: "<file path>",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name:  "url",
-								Value: "https://naptan.api.dft.gov.uk/v1/access-nodes?dataFormat=xml",
-								Usage: "HTTP location of the XML file",
-							},
-						},
-						Action: func(c *cli.Context) error {
-							url := c.String("url")
-							log.Info().Msgf("NaPTAN HTTP import from %s", url)
-
-							resp, err := http.Get(url)
-
-							if err != nil {
-								log.Fatal().Err(err)
-							}
-							defer resp.Body.Close()
-
-							naptanDoc, err := naptan.ParseXMLFile(resp.Body, naptan.BusFilter)
-
-							if err != nil {
-								return err
-							}
-
-							naptanDoc.ImportIntoMongoAsCTDF(&ctdf.DataSource{
-								Provider: "Department of Transport",
-								Dataset:  url,
-							})
-
-							return nil
-						},
-					},
-				},
-			},
-			{
-				Name:  "traveline-noc",
-				Usage: "Traveline National Operator Code data",
-				Subcommands: []*cli.Command{
-					{
-						Name:      "import-file",
-						Usage:     "import a local XML file",
-						ArgsUsage: "<file path>",
-						Action: func(c *cli.Context) error {
-							if c.Args().Len() == 0 {
-								return errors.New("file path must be provided")
-							}
-
-							filePath := c.Args().Get(0)
-
-							log.Info().Msgf("Traveline NOC file import from %s", filePath)
-
-							file, err := os.Open(filePath)
-							if err != nil {
-								log.Fatal().Err(err)
-							}
-							defer file.Close()
-
-							travelineData, err := travelinenoc.ParseXMLFile(file)
+						case "traveline-noc":
+							log.Info().Msgf("Traveline NOC file import from %s", dataFile.Name)
+							travelineData, err := travelinenoc.ParseXMLFile(dataFile.Reader)
 
 							if err != nil {
 								return err
 							}
 
 							travelineData.ImportIntoMongoAsCTDF(&ctdf.DataSource{
-								Dataset: fmt.Sprintf("local-file:%s", filePath),
+								Dataset: dataFile.Name,
 							})
-
-							return nil
-						},
-					},
-					{
-						Name:  "import-http",
-						Usage: "import a remote XML file over HTTP",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name:  "url",
-								Value: "https://www.travelinedata.org.uk/noc/api/1.0/nocrecords.xml",
-								Usage: "HTTP location of the XML file",
-							},
-						},
-						Action: func(c *cli.Context) error {
-							url := c.String("url")
-							log.Info().Msgf("Traveline NOC HTTP import from %s", url)
-
-							resp, err := http.Get(url)
-
-							if err != nil {
-								log.Fatal().Err(err)
-							}
-							defer resp.Body.Close()
-
-							travelineData, err := travelinenoc.ParseXMLFile(resp.Body)
+						case "transxchange":
+							log.Info().Msgf("TransXChange file import from %s ", dataFile.Name)
+							transXChangeDoc, err := transxchange.ParseXMLFile(dataFile.Reader)
 
 							if err != nil {
 								return err
 							}
 
-							travelineData.ImportIntoMongoAsCTDF(&ctdf.DataSource{
-								Dataset: url,
+							transXChangeDoc.ImportIntoMongoAsCTDF(&ctdf.DataSource{
+								Provider: "Department of Transport", // This may not always be true
+								Dataset:  dataFile.Name,
 							})
+						default:
+							return errors.New(fmt.Sprintf("Unsupport format %s", format))
+						}
+					}
 
-							return nil
-						},
-					},
-				},
-			},
-			{
-				Name:  "transxchange",
-				Usage: "TransXChange bus route data",
-				Subcommands: []*cli.Command{
-					{
-						Name:      "import-file",
-						Usage:     "import an XML file",
-						ArgsUsage: "<file path>",
-						Action: func(c *cli.Context) error {
-							if c.Args().Len() == 0 {
-								return errors.New("file path must be provided")
-							}
-
-							filePath := c.Args().Get(0)
-							fileExtension := filepath.Ext(filePath)
-
-							files := []DataFile{}
-
-							if fileExtension == ".xml" {
-								pretty.Println("lovely xml file")
-
-								file, err := os.Open(filePath)
-								if err != nil {
-									log.Fatal().Err(err)
-								}
-								defer file.Close()
-
-								files = append(files, DataFile{
-									Name:   "",
-									Reader: file,
-								})
-							} else if fileExtension == ".zip" {
-								pretty.Println("lovely zip file")
-
-								archive, err := zip.OpenReader(filePath)
-								if err != nil {
-									panic(err)
-								}
-								defer archive.Close()
-
-								for _, zipFile := range archive.File {
-									file, _ := zipFile.Open()
-
-									files = append(files, DataFile{
-										Name:   zipFile.Name,
-										Reader: file,
-									})
-								}
-							} else {
-								return errors.New(fmt.Sprintf("Unsupport file extension %s", fileExtension))
-							}
-
-							for _, file := range files {
-								log.Info().Msgf("TransXChange file import from %s %s", filePath, file.Name)
-
-								transXChangeDoc, err := transxchange.ParseXMLFile(file.Reader)
-
-								if err != nil {
-									return err
-								}
-
-								transXChangeDoc.ImportIntoMongoAsCTDF(&ctdf.DataSource{
-									Dataset: fmt.Sprintf("local-file:%s %s", filePath, file.Name),
-								})
-							}
-
-							return nil
-						},
-					},
+					return nil
 				},
 			},
 		},
@@ -269,4 +161,18 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
+}
+
+func isValidUrl(toTest string) bool {
+	_, err := url.ParseRequestURI(toTest)
+	if err != nil {
+		return false
+	}
+
+	u, err := url.Parse(toTest)
+	if err != nil || u.Scheme == "" || u.Host == "" {
+		return false
+	}
+
+	return true
 }
