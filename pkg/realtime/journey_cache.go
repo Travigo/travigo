7844ec393f338cfcc5d380be80b545f8c286010d
Aaron Claydon <claydon.aaron@gmail.com>
2022-04-16 00:36:39 +0100

    Fix redis cache marshalling (Realtime Redis shared cache not working #37)

--- pkg/realtime/journey_cache.go
+++ pkg/realtime/journey_cache.go
@@ -1,7 +1,15 @@
 package realtime
 
-import "github.com/britbus/britbus/pkg/ctdf"
+import (
+	"encoding/json"
+
+	"github.com/britbus/britbus/pkg/ctdf"
+)
 
 type CacheJourney struct {
 	Path []*ctdf.JourneyPathItem `groups:"detailed"`
+}
+
+func (j CacheJourney) MarshalBinary() ([]byte, error) {
+	return json.Marshal(j)
 }
