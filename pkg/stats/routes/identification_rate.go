40f456af486e8cf7325c81b1fa49c512b77d3a72
Aaron Claydon <claydon.aaron@gmail.com>
2023-02-15 17:30:40 +0000

    Get other operator identifiers in identification rate search

--- pkg/stats/routes/identification_rate.go
+++ pkg/stats/routes/identification_rate.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"context"
 	"strings"
 
+	"github.com/britbus/britbus/pkg/ctdf"
+	"github.com/britbus/britbus/pkg/database"
 	"github.com/britbus/britbus/pkg/stats"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func IdentificationRateRouter(router fiber.Router) {
@@ -17,6 +21,17 @@ func getIdentificationRate(c *fiber.Ctx) error {
 	var operatorsList []string
 	if operatorsListString != "" {
 		operatorsList = strings.Split(operatorsListString, ",")
+
+		// Get the other identifiers for each of the operators as we have many different forms (eg. NOCID or NOC)
+		for _, operatorID := range operatorsList {
+			operatorsCollection := database.GetCollection("operators")
+			var operator *ctdf.Operator
+			operatorsCollection.FindOne(context.Background(), bson.M{"primaryidentifier": operatorID}).Decode(&operator)
+
+			if operator != nil {
+				operatorsList = append(operatorsList, operator.OtherIdentifiers...)
+			}
+		}
 	}
 
 	rateStats := stats.GetIdentificationRateStats(operatorsList)
