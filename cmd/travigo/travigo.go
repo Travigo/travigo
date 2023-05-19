531fd4d769ffd5c2717e70b48760844b85701379
Aaron Claydon <claydon.aaron@gmail.com>
2023-05-19 22:35:44 +0100

    Move web-api into single travigo binary #91

---
+++ cmd/travigo/travigo.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/travigo/travigo/pkg/api"
+	"github.com/travigo/travigo/pkg/database"
+	"github.com/travigo/travigo/pkg/elastic_client"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/travigo/travigo/pkg/transforms"
+	"github.com/urfave/cli/v2"
+
+	_ "time/tzdata"
+)
+
+func main() {
+	if os.Getenv("TRAVIGO_LOG_FORMAT") != "JSON" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+	}
+
+	if os.Getenv("TRAVIGO_DEBUG") == "YES" {
+		log.Logger = log.Logger.Level(zerolog.DebugLevel)
+	} else {
+		log.Logger = log.Logger.Level(zerolog.InfoLevel)
+	}
+
+	transforms.SetupClient()
+
+	if err := database.Connect(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to database")
+	}
+
+	if err := elastic_client.Connect(false); err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to Elasticsearch")
+	}
+
+	app := &cli.App{
+		Name:        "travigo",
+		Description: "Single binary of truth for Travigo - runs all the services",
+
+		Commands: []*cli.Command{
+			api.RegisterCLI(),
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+}
---
+++ pkg/api/cli.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"github.com/travigo/travigo/pkg/ctdf"
+	"github.com/travigo/travigo/pkg/dataaggregator"
+	"github.com/urfave/cli/v2"
+)
+
+func RegisterCLI() *cli.Command {
+	return &cli.Command{
+		Name:  "web-api",
+		Usage: "Provides the core web API",
+		Subcommands: []*cli.Command{
+			{
+				Name:  "run",
+				Usage: "run web api server",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "listen",
+						Value: ":8080",
+						Usage: "listen target for the web server",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					dataaggregator.GlobalSetup()
+
+					ctdf.LoadSpecialDayCache()
+
+					SetupServer(c.String("listen"))
+
+					return nil
+				},
+			},
+		},
+	}
+}
