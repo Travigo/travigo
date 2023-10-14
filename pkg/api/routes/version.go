4d1bc7660983f446a7631b62d7f9eba93491685d
Aaron Claydon <claydon.aaron@gmail.com>
2023-10-14 22:32:59 +0100

    Update version API

--- pkg/api/routes/version.go
+++ pkg/api/routes/version.go
@@ -1,9 +1,27 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"runtime/debug"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func APIVersion(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"version": "v0.1",
-	})
+	buildInfo, _ := debug.ReadBuildInfo()
+
+	versionMap := fiber.Map{
+		"version":   "v0.1",
+		"goversion": buildInfo.GoVersion,
+	}
+
+	for _, kv := range buildInfo.Settings {
+		if kv.Value == "" {
+			continue
+		}
+		if kv.Key == "vcs.revision" {
+			versionMap["commit"] = kv.Value
+		}
+	}
+
+	return c.JSON(versionMap)
 }
