cd616f8f1d50f732789d3d17a668f2fb3dc436ba
Aaron Claydon <claydon.aaron@gmail.com>
2023-12-11 18:40:19 +0000

    Train alerts

--- pkg/realtime/nationalrail/darwin/trainalert.go
+++ pkg/realtime/nationalrail/darwin/trainalert.go
@@ -1,4 +1,26 @@
 package darwin
 
 type TrainAlert struct {
+	AlertID string
+
+	SendAlertBySMS     bool
+	SendAlertByEmail   bool
+	SendAlertByTwitter bool
+
+	AlertServices []TrainAlertServices `xml:"AlertServices>AlertService"`
+
+	Source string
+
+	AlertText string
+
+	Audience  string
+	AlertType string
+}
+
+type TrainAlertServices struct {
+	RID string `xml:",attr"`
+	UID string `xml:",attr"`
+	SSD string `xml:",attr"`
+
+	Locations []string `xml:"Location"`
 }
