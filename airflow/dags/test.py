cf12234012e80ac2789fcd9da3df7fe598545449
Aaron Claydon <claydon.aaron@gmail.com>
2024-12-17 23:01:52 +0000

    test

--- airflow/dags/test.py
+++ airflow/dags/test.py
@@ -1,17 +1,34 @@
-from datetime import datetime
+"""
+This is an example dag for using the KubernetesPodOperator.
+"""
 
+from kubernetes.client import models as k8s
 from airflow import DAG
-from airflow.decorators import task
-from airflow.operators.bash import BashOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
 
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
-    # Tasks are represented as operators
-    hello = BashOperator(task_id="hello", bash_command="echo hello")
+default_args = {
+    'owner': 'airflow'
+}
 
-    @task()
-    def airflow():
-        print("airflow")
-
-    # Set dependencies between tasks
-    hello >> airflow()
+with DAG(
+    dag_id='k8s-example-0',
+    default_args=default_args,
+    schedule_interval="*/5 * * * *",
+    start_date=days_ago(2),
+    catchup=False,
+    tags=['k8s-pod-operator','example'],
+) as dag:
+    k = KubernetesPodOperator(
+        namespace='airflow',
+        image="ubuntu:latest",
+        cmds=["bash", "-cx"],
+        arguments=["echo hello"],
+        name="k8s-pod",
+        task_id="task",
+        is_delete_operator_pod=True,
+        hostnetwork=False,
+        startup_timeout_seconds=1000
+    )
