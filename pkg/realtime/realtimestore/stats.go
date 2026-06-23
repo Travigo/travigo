a20d1e413338333c3635196524d5370f246d6ed6
Aaron Claydon <claydon.aaron@gmail.com>
2026-06-23 21:02:28 +0100

    move stats to redis

--- pkg/realtime/realtimestore/stats.go
+++ pkg/realtime/realtimestore/stats.go
@@ -1,9 +1,11 @@
 package realtimestore
 
 import (
+	"context"
 	"strings"
 
 	"github.com/travigo/travigo/pkg/ctdf"
+	"github.com/travigo/travigo/pkg/redis_client"
 )
 
 type RealtimeJourneyStats struct {
@@ -35,102 +37,54 @@ func GetRealtimeJourneys() RealtimeJourneyStats {
 	datasources := map[string]int{}
 	providers := map[string]int{}
 
-	// realtimeActiveCutoffDate := ctdf.GetActiveRealtimeJourneyCutOffDate()
+	ctx := context.Background()
 
-	// matchStage := bson.D{
-	// 	{
-	// 		Key: "$match",
-	// 		Value: bson.D{
-	// 			{Key: "modificationdatetime", Value: bson.M{"$gt": realtimeActiveCutoffDate}},
-	// 			{Key: "activelytracked", Value: true},
-	// 		},
-	// 	},
-	// }
-	// //lookupStage := bson.D{
-	// //	{
-	// //		"$lookup",
-	// //		bson.M{
-	// //			"from":         "services",
-	// //			"localField":   "journey.serviceref",
-	// //			"foreignField": "primaryidentifier",
-	// //			"as":           "_journeyservice",
-	// //		},
-	// //	},
-	// //}
-	// //addFieldStage := bson.D{
-	// //	{
-	// //		"$addFields",
-	// //		bson.M{"journey.service": bson.M{"$first": "$_journeyservice"}},
-	// //	},
-	// //}
-	// projectStage := bson.D{
-	// 	{
-	// 		Key: "$project",
-	// 		Value: bson.D{
-	// 			bson.E{Key: "primaryidentifier", Value: 1},
-	// 			bson.E{Key: "activelytracked", Value: 1},
-	// 			bson.E{Key: "timeoutdurationminutes", Value: 1},
-	// 			bson.E{Key: "modificationdatetime", Value: 1},
-	// 			bson.E{Key: "reliability", Value: 1},
-	// 			bson.E{Key: "datasource.datasetid", Value: 1},
-	// 			bson.E{Key: "datasource.providerid", Value: 1},
-	// 			bson.E{Key: "service.transporttype", Value: 1},
-	// 			// bson.E{Key: "vehiclelocation", Value: 1},
-	// 			bson.E{Key: "journey.path.destinationstopref", Value: 1},
-	// 			bson.E{Key: "journey.path.destinationarrivaltime", Value: 1},
-	// 			bson.E{Key: "occupancy.occupancyavailable", Value: 1},
-	// 			bson.E{Key: "occupancy.actualvalues", Value: 1},
-	// 			bson.E{Key: "detailedrailinformation.carriages.id", Value: 1},
-	// 		},
-	// 	},
-	// }
-	// activeRealtimeJourneys, _ := realtimeJourneysCollection.Aggregate(context.Background(), mongo.Pipeline{matchStage, projectStage})
+	iter := redis_client.Client.Scan(ctx, 0, realtimeJourneyDetailsKey("*"), 0).Iterator()
 
-	// for activeRealtimeJourneys.Next(context.Background()) {
-	// 	var realtimeJourney ctdf.RealtimeJourney
-	// 	err := activeRealtimeJourneys.Decode(&realtimeJourney)
+	for iter.Next(ctx) {
+		realtimeJourney, err := FindByIdentifier(ctx, iter.Val())
 
-	// 	if err != nil {
-	// 		continue
-	// 	}
+		if err != nil || realtimeJourney == nil {
+			continue
+		}
 
-	// 	if realtimeJourney.IsActive() {
-	// 		if realtimeJourney.ActivelyTracked {
-	// 			numberActiveRealtimeJourneys += 1
+		if realtimeJourney.IsActive() {
+			if realtimeJourney.ActivelyTracked {
+				numberActiveRealtimeJourneys += 1
 
-	// 			if realtimeJourney.Reliability == ctdf.RealtimeJourneyReliabilityLocationWithTrack {
-	// 				numberActiveRealtimeJourneysWithTrack += 1
-	// 			}
-	// 			if realtimeJourney.Reliability == ctdf.RealtimeJourneyReliabilityLocationWithoutTrack {
-	// 				numberActiveRealtimeJourneysWithoutTrack += 1
-	// 			}
-	// 			if realtimeJourney.Reliability == ctdf.RealtimeJourneyReliabilityExternalProvided {
-	// 				numberActiveRealtimeJourneysExternal += 1
-	// 			}
+				if realtimeJourney.Reliability == ctdf.RealtimeJourneyReliabilityLocationWithTrack {
+					numberActiveRealtimeJourneysWithTrack += 1
+				}
+				if realtimeJourney.Reliability == ctdf.RealtimeJourneyReliabilityLocationWithoutTrack {
+					numberActiveRealtimeJourneysWithoutTrack += 1
+				}
+				if realtimeJourney.Reliability == ctdf.RealtimeJourneyReliabilityExternalProvided {
+					numberActiveRealtimeJourneysExternal += 1
+				}
 
-	// 			if realtimeJourney.Service != nil {
-	// 				transportTypes[realtimeJourney.Service.TransportType] += 1
-	// 			}
+				if realtimeJourney.Service != nil {
+					transportTypes[realtimeJourney.Service.TransportType] += 1
+				}
 
-	// 			datasources[realtimeJourney.DataSource.DatasetID] += 1
-	// 			providers[realtimeJourney.DataSource.ProviderID] += 1
+				datasources[realtimeJourney.DataSource.DatasetID] += 1
+				providers[realtimeJourney.DataSource.ProviderID] += 1
 
-	// 			// Features
-	// 			if realtimeJourney.Occupancy.OccupancyAvailable {
-	// 				features["Occupancy"] += 1
+				// Features
+				if realtimeJourney.Occupancy.OccupancyAvailable {
+					features["Occupancy"] += 1
 
-	// 				if realtimeJourney.Occupancy.ActualValues {
-	// 					features["OccupancyActualValues"] += 1
-	// 				}
-	// 			}
-	// 			if len(realtimeJourney.DetailedRailInformation.Carriages) > 0 {
-	// 				features["DetailedRailInformationCarriages"] += 1
-	// 			}
-	// 		} else {
-	// 			numberActiveRealtimeJourneysNotActivelyTracked += 1
-	// 		}
-	// 	}
-	// }
+					if realtimeJourney.Occupancy.ActualValues {
+						features["OccupancyActualValues"] += 1
+					}
+				}
+				if len(realtimeJourney.DetailedRailInformation.Carriages) > 0 {
+					features["DetailedRailInformationCarriages"] += 1
+				}
+			} else {
+				numberActiveRealtimeJourneysNotActivelyTracked += 1
+			}
+		}
+	}
 
 	return RealtimeJourneyStats{
 		Current:              numberActiveRealtimeJourneys,
